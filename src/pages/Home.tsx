e4753871e848296aa55998ef60698cab8ce3edc4
T_Mori <morit0301@gmail.com>
2025-01-01 21:07:29 +0900

    Componentize Home.tsx and implement the Monthly Summary component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import { Box } from "@mui/material";
+import MonthlySummary from "../components/MonthlySummary";
+import Calender from "../components/Calender";
+import TransactionMenu from "../components/TransactionMenu";
+import TransactionForm from "../components/TransactionForm";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box sx={{ display: "flex" }}>
+      {/* left side */}
+      <Box sx={{ flexGrow: 1 }}>
+        <MonthlySummary />
+        <Calender />
+      </Box>
+      {/* right side */}
+      <Box>
+        <TransactionMenu />
+        <TransactionForm />
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
