e76a961d76e35ce38e9ed2482e7031295c7ca9ef
T_Mori <morit0301@gmail.com>
2025-01-02 04:09:56 +0900

    Implementing data retrieval from Firestore

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,34 @@ import AppLayout from "./components/layout/AppLayout";
 import { theme } from "./theme/theme";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Transaction } from "./types";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, " => ", doc.data());
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+        console.log(transactionsData);
+        setTransactions(transactionsData);
+      } catch (err) {
+        //err
+      }
+    };
+    fetchTransactions();
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
