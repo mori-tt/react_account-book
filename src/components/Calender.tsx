6b04873852c8a108ce6dbf749d444fe07667f504
T_Mori <morit0301@gmail.com>
2025-01-02 20:14:48 +0900

    FullCalendar settings and initial display

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,5 +1,46 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import "../calendar.css";
+import { EventContentArg } from "@fullcalendar/core/index.js";
+
 const Calender = () => {
-  return <div>Calender</div>;
+  const events = [
+    { title: "React Hands-on 'Tanstack Query'", start: "2025-01-05" },
+    {
+      title: "React Hands-on 'useMemo, useCallback'",
+      start: "2025-01-12",
+      income: 300,
+      expense: 200,
+      balance: 100,
+    },
+  ];
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    );
+  };
+  return (
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
 };
 
 export default Calender;
